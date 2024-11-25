73d015466503019322f514152d2f0300ff22177e
Hamza <hamzacoude@gmail.com>
2024-11-25 23:02:51 +0100

    #2 create slaider to header

---
+++ apk.js
@@ -0,0 +1,26 @@
+let images = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+
+function showNextImage() {
+    images[currentIndex].classList.remove('opacity-30');
+    images[currentIndex].classList.add('opacity-30');
+
+    let oldIndex = currentIndex;
+    setTimeout(() => {
+        images[oldIndex].classList.add('hidden');
+    }, 500);
+
+    currentIndex = (currentIndex + 1) % images.length;
+
+    setTimeout(() => {
+        images[currentIndex].classList.remove('hidden');
+        setTimeout(() => {
+            images[currentIndex].classList.add('opacity-30');
+            images[currentIndex].classList.remove('opacity-0');
+        }, 10);
+    }, 500);
+
+}
+
+setInterval(showNextImage, 2000);
